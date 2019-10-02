f61fd10310df2d073db4476cbdb0f16b3381e690
Sam Forrester <SForrester@spartaglobal.com>
2019-10-02 20:57:58 +0100

    made a run file that writes information to a text file

--- Postcode_to_txt_define.py
+++ Postcode_to_txt_define.py
@@ -1,14 +1,30 @@
+# Import the necessaries
 import requests
 import json
 
+# Define function to change API URL to json dictionary
 def postcode_to_json(postcode, url):
-    resp = requests.get(url+postcode)
-    return resp.json()
+    try:
+        resp = requests.get(url+postcode)
+        return resp.json()
+    except:
+        pass
 
+# Define function to change API URL to text
 def postcode_to_text(postcode, url):
-    resp = requests.get(url + postcode)
-    return resp.text()
+    resp = requests.get(url+postcode)
+    return resp.text
 
+# Define function to get the status code of a URL
 def postcode_status_request(postcode, url):
-    resp = requests.get(url + postcode)
-    return resp.status_code()
+    resp = requests.get(url+postcode)
+    return resp.status_code
+
+def write_to_file(file, order_item):
+    try:
+        with open(file, 'a') as opened_file:
+            opened_file.write(order_item + '\n')
+
+        opened_file.close()
+    except FileNotFoundError:
+        print('File not Found')
